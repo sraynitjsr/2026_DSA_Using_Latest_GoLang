703547a41f534ae1acf8401ce648e3cc1b7c442b
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-04-11 03:42:18 +0530

    Using New Features of GoLang to Solve DSA Problems
    Using New Features of GoLang to Solve DSA Problems

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	blind75 "sraynitjsr/Blind75"
+	latestgolang "sraynitjsr/Core_GoLang_Latest_Features"
 	neetcode150 "sraynitjsr/NeetCode150"
 )
 
 func main() {
-	fmt.Println("Using New Features of GoLang to Solve DSA Problems, 2026")
+	fmt.Println("Using New Features of GoLang to Solve DSA Problems")
+
+	latestgolang.LatestGoLangFeatures()
+
 	blind75.Blind75()
+
 	neetcode150.NeetCode150()
 }
